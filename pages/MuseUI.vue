d8d27dc9a82f67ef8139e4667713da0cb2394c63
Cody O'Dell <codyodell@gmail.com>
2018-04-29 04:45:19 -0500

    ...

--- pages/MuseUI.vue
+++ pages/MuseUI.vue
@@ -1,35 +1,13 @@
 <template>
   <div>
-    <mu-content-block>
-      <mu-paper class="demo-menu">
-        <mu-menu desktop>
-          <mu-menu-item title="Bold" afterText="⌘B"/>
-          <mu-menu-item title="Italic" afterText="⌘I"/>
-          <mu-menu-item title="Underline" afterText="⌘U"/>
-          <mu-menu-item title="Strikethrough" afterText="Alt+Shift+5"/>
-          <mu-menu-item title="Superscript" afterText="⌘."/>
-          <mu-menu-item title="Subscript" afterText="⌘,"/>
-          <mu-divider />
-          <mu-menu-item title="Paragraph styles" rightIcon="keyboard_arrow_right"/>
-          <mu-menu-item title="Align" rightIcon="keyboard_arrow_right"/>
-          <mu-menu-item title="Line spacing" rightIcon="keyboard_arrow_right"/>
-          <mu-menu-item title="Paragraph styles" rightIcon="keyboard_arrow_right"/>
-          <mu-menu-item title="Numbered list" rightIcon="keyboard_arrow_right"/>
-          <mu-menu-item title="List options" rightIcon="keyboard_arrow_right"/>
-          <mu-divider />
-          <mu-menu-item title="Clear formatting" afterText="⌘/"/>
-        </mu-menu>
-      </mu-paper>
-      <mu-paper class="demo-menu">
-        <mu-menu desktop :width="256">
-          <mu-menu-item title="Open" afterText="Cmd + O" />
-          <mu-menu-item title="Paste in place" afterText="Shift + V" />
-          <mu-menu-item title="Research" afterText="Opt + Shift + Cmd + I" />
-        </mu-menu>
-      </mu-paper>
-    </mu-content-block>
+
   </div>
 </template>
 <script>
 export default {}
 </script>
+<style lang="scss">
+body {
+	background: red;
+}
+</style>
